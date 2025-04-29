45b13db6e637c93c9951603602e6e353cb4b5354
cnvt1803 <vantruongasds@gmail.com>
2025-04-29 18:13:27 +0700

    commit

--- backend/app/jobs/tasks.py
+++ backend/app/jobs/tasks.py
@@ -4,7 +4,7 @@ from app.database import SessionLocal
 from app.models.param_data import ParamData
 
 USERNAME = ""
-KEY = ""
+KEY
 BASE_URL = f"https://io.adafruit.com/api/v2/{USERNAME}/feeds"
 # FEED_ID = "bbc-temp"
 
